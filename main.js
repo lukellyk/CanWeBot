bb0ad41727d4227db67df22458e009b556a408be
lukellyk <luke-k@live.com>
2021-07-23 00:03:27 +1000

    setting up commands

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
+const fs = require('fs');
+//require discord.js module
 const Discord = require('discord.js');
-
+//create new discord client
 const client = new Discord.Client();
 
-const dotenv = require('dotenv');
-dotenv.config();
+//load commands
+client.commands = new Discord.Collection();
 
-const prefix = '!'
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
+
+//retrieve prefix from config file
+const { prefix, token } = require('./config.json');
+
+//log status when online
 client.once('ready', () => {
     console.log('CanWeBot is online!');
 });
 
 client.on('message', message => {
-    if(message.content.startsWith(prefix) || message.author.bot) return;
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
 
-    if (command === 'ping'){
-        message.channel.send('Pong!')
-    }
 });
 
-
-
-
-
-
-client.login(process.env.TOKEN);
+//login using token
+client.login(token);
